21ed163e74f67e7bb56726b889a7ccf49dc3c4b8
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2023-09-17 14:05:54 +0530

    first commit

---
+++ dist/Logger.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Logger {
+    log(val) {
+        console.log(val);
+    }
+}
+exports.default = Logger;
---
+++ src/Logger.ts
@@ -0,0 +1,5 @@
+export default class Logger {
+	log(val: any) {
+		console.log(val);
+	}
+}
